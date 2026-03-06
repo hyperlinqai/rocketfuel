6b94b097c2268d535637585acab41faec72f5c48
hyperlinqai <hyperlinqtechnology@gmail.com>
2026-03-06 10:45:22 +0530

    Enhance security: Update Referrer-Policy to strict-origin-when-cross-origin

--- next.config.ts
+++ next.config.ts
@@ -34,7 +34,7 @@ const nextConfig = {
           },
           {
             key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin',
+            value: 'strict-origin-when-cross-origin',
           },
           {
             key: 'Strict-Transport-Security',
